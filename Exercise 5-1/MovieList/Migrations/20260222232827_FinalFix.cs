44bdb99bbacce00b8ca9b6cab347d6f2a3d60eaf
jfiffe1 <jfiffe1@live.spcollege.edu>
2026-02-23 16:53:24 -0500

    Add files via upload

---
+++ Exercise 5-1/MovieList/Migrations/20260222232827_FinalFix.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieList.Migrations
+{
+    public partial class FinalFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 4);
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "GenreId", "Name", "Rating", "Year" },
+                values: new object[] { 1, "D", "Casablanca", 5, 1942 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 1);
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "GenreId", "Name", "Rating", "Year" },
+                values: new object[] { 4, "D", "Casablanca", 5, 1943 });
+        }
+    }
+}
